83f56a25fa83b4856c3dd02029b46d81876f3e59
lzx <2061105660@qq.com>
2026-01-15 17:29:04 +0800

    添加 ftxui库

--- src/xcore/main.cpp
+++ src/xcore/main.cpp
@@ -1,27 +1,37 @@
+#include "catch2/catch.hpp"
+#include "ftxui/dom/elements.hpp"
+#include "ftxui/screen/screen.hpp"
+#include "plugin_interface.h"
+#include "json/json.hpp"
+#include <Windows.h>
 #include <iostream>
 #include <libloaderapi.h>
-#include <plugin_interface.h>
-#include <Windows.h>
 #include <winnt.h>
-#include"json/json.hpp"
-#include "catch2/catch.hpp"
-int main() {
-    std::cout << "Hello World from xcore" << std::endl;
-    
-    auto p1=LoadLibrary(TEXT("plugins/p1.dll"));
-    if(p1!=NULL) {
-        auto get_name=(const char* (*)())GetProcAddress(p1, "get_name");
-        if(get_name) {
-            std::cout << "Plugin1 name: " << get_name() << std::endl;
-        }else{
-            std::cout << "get_name not found" << std::endl;
-        }
-    }else{
-        std::cout << "Plugin1 load failed" << std::endl;
-    }
-    
 
-    return 0;
+int main() {
+
+  // auto p1=LoadLibrary(TEXT("plugins/p1.dll"));
+  // if(p1!=NULL) {
+  //     auto get_name=(const char* (*)())GetProcAddress(p1, "get_name");
+  //     if(get_name) {
+  //         std::cout << "Plugin1 name: " << get_name() << std::endl;
+  //     }else{
+  //         std::cout << "get_name not found" << std::endl;
+  //     }
+  // }else{
+  //     std::cout << "Plugin1 load failed" << std::endl;
+  // }
+
+  using namespace ftxui;
+  Element documentation = hbox({
+      text("left") | border,
+      text("middle") | border|flex,
+      text("right") | border,
+  });
+  auto screen=Screen::Create(Dimension::Full(),Dimension::Fit(documentation));
+  Render(screen,documentation);
+  screen.Print();
+  return 0;
 }
 
 // TEST_CASE("T1"){
